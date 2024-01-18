72c9d207b6dbd719774998e83734c7ec29774bc7
Enriipy <carlieep@gmail.com>
2024-01-18 18:38:10 -0300

    Cambiamos de nombre el leeme

--- teorema_pitagoras.js
+++ teorema_pitagoras.js
@@ -1,5 +1,5 @@
-let A = prompt('Imprimir valor de a');
-let B = prompt('Imprimir valor de b');
+let A = prompt('Colocar el valor de a');
+let B = prompt('Colocar el valor de b');
 let expa = A ** 2;
 let expb = B ** 2;
 let suma = expa + expb;
