03da0b76938e15125315d16ed821880f810d305a
Enriipy <carlieep@gmail.com>
2024-01-22 19:42:18 -0300

    arrays

--- usando_arrays.js
+++ usando_arrays.js
@@ -2,13 +2,14 @@
 let unaVariable = "naranja";
 
 // Arrays
-let razasDePerros = ("Labrador", "Pastor Aleman", "Chiguagua", "Pitbul");
+let razasDePerros = ["Labrador", "Pastor Aleman", "Chiguagua", "Pitbul"];
 
-const ultimoElemento = razasDePerros(razasDePerros.length - 1);
+const ultimoElemento = razasDePerros.length - 1;
 console.log(ultimoElemento);
 
 // Sobreescribiendo un array en la posicion -36
 razasDePerros[-36] = null;
+
 
 razasDePerros[2] = "Dalmata";
 
@@ -22,3 +23,8 @@ console.log(razasDePerros[-36]);
 const cantidadRazaDePerros = razasDePerros.length;
 console.log(razasDePerros.length);
 console.log(cantidadRazaDePerros);
+
+// Agregando nuevos elementos al array
+razasDePerros.push("Delmer");
+console.log({razasDePerros});
+console.log(`Hemos agregado un delmer a las raza ${razasDePerros}`);
