9a8eb1270a8a6b5a2e60ed097cb5f9c0cdc2ed4f
Enriipy <carlieep@gmail.com>
2024-01-21 22:06:55 -0300

    Revisar actualización

--- Proyecto_2.js
+++ Proyecto_2.js
@@ -6,7 +6,10 @@ La distancia de 130 Km es igual a 209.2142 millas
 
 Referencia, 1 milla es igual a 1.60934 kilómetro */
 
-let milla_1 = m;
-console.log(`Ingrese el valor de la milla ${m}`)
-let km = m * 1.60934;
-console.log(`La distancia de millas es igual a kilómetros ${km.toFixed(4)}`);
+const milla = prompt("Ingrese la distancia en millas:");
+const Km = milla * 1.60934;
+if (!isNaN(Km)){
+    console.log(`La distancia de ${milla} millas es igual a ${Km.toFixed(4)} kilómetros.`);
+    console.log(`La distancia de ${milla} millas es igual a ${Km}`);
+    alert("Por favor, ingrese un número válido para la distancia en millas.");
+}
