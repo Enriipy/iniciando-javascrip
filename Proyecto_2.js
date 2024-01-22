09de0441da2ed8566b1e7043a498b4c801263590
Enriipy <carlieep@gmail.com>
2024-01-21 21:43:26 -0300

    Proyecto numero 2

--- Proyecto_2.js
+++ Proyecto_2.js
@@ -1,9 +1,13 @@
-/**
- * Proyecto 2
+/** Proyecto 2
 Convertidor de millas a kilómetros
 Crea una variable que contenga un valor en millas, conviértelo a kilómetros, muestra el mensaje en consola del valor en kilómetros, el formato a mostrar será:
 
 La distancia de 130 Km es igual a 209.2142 millas
 
-Referencia, 1 milla es igual a 1.60934 kilómetros
- */
+Referencia, 1 milla es igual a 1.60934 kilómetro */
+
+let milla_1 = a;
+console.log(`Ingrese el valor de la milla ${a}`)
+let km = a * 1.60934;
+
+console.log(`La distancia de ${km.toFixed(4)} * ${a} Km es igual a millas`);
