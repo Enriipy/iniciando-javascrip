4393a4175068e948fd080cbdc5ed3b7b24bf2c8a
Enriipy <carlieep@gmail.com>
2024-01-24 18:54:46 -0300

    Ejemplos primarios de funciones para find

---
+++ usando_find.js
@@ -0,0 +1,16 @@
+// Definiendo la función
+// Funciones normales
+function decirHola() {
+    // Bueno, aqui dentro vas a 
+    // escribir las sentencias de tu programa o
+    // algoritmo
+    console.log("Hola");
+}
+
+function otraFuncion() {
+    alert("Soy una alerta");
+}
+
+// Invoco, llamo, call
+decirHola();
+otraFuncion();
