84a372d00c3b07681c6fb7c226807e0ad8981070
Enriipy <carlieep@gmail.com>
2024-01-25 19:03:25 -0300

    Arrays multidimensional ejercio y prueba

--- Arrays_Multidimensionales.js
+++ Arrays_Multidimensionales.js
@@ -1,4 +1,20 @@
+/**Ejercicio 3.3
+Crear un array que contenga tres valores, 1, 2, 3.
+Anidar el array original en un nuevo array, tres veces.
+Mostrar en pantalla el valor 2 de uno de los arrays en la consola */
+
 let someValues = [1, 2, 3];
-let someValues2 = [4, 5, 6];
-let somevalues3 = [7, 8, 9];
-let arrOfArrays = [someValues, someValues2, somevalues3];
+let arrOfArrays = [someValues];
+console.log(arrOfArrays[0][1]);
+
+/**Pueba */
+
+let someV1 = [1, 2, 3];
+let someV2 = [4, 5, 6];
+let someV3 = [7, 8, 9];
+let arrOfArray = [someV1, someV2, someV3];
+
+let arrOfArray2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let value1 = arrOfArray[0][1];
+let arrOfArrayOfArrays = [arrOfArray, arrOfArray, arrOfArray];
+let middleValue = arrOfArrayOfArrays[1][1][1];
