690524a51c2645ad27e6c9c04218c0c521a6d677
Enriipy <carlieep@gmail.com>
2024-01-25 15:38:04 -0300

    Ejemplos primarios de funciones para find

--- usando_find.js
+++ usando_find.js
@@ -11,6 +11,28 @@ function otraFuncion() {
     alert("Soy una alerta");
 }
 
+// Lo que esta dentro del parentesis se llama parametro, y es por defecto
+function decirNombre(nombre="no puso el nombre") {
+    console.log(`El nombre ingresado es ${nombre}`);
+}
+
+function dameElNombreDeMiMascota (nombre) {
+    return nombre;
+}
+// funciones anonimas
+const recibiendoFuncionAnonima = function() {console.log("Soy una funcion anomima")};
+
+
 // Invoco, llamo, call
 decirHola();
 otraFuncion();
+recibiendoFuncionAnonima();
+// Lo que esta dentro del parentesis se llama argumento
+decirNombre("Abigails");
+decirNombre();
+const nombreDeMascota = dameElNombreDeMiMascota("Firulais");
+
+// Al fin vamos a usar find
+const frutas = ("Pomelo", "Manzana", "Banana");
+const frutaEncontrada = frutas.find(function (frutas) { return frutas === "Pomelo"});
+console.log(frutaEncontrada);
